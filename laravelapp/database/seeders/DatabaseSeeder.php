66b1cbde53033512ac49712ebc548fe770d7b43c
Red-Fox-98 <evgeniya.zaitseva.98@gmail.com>
2022-09-05 15:00:10 +0600

    Update

--- laravelapp/database/seeders/DatabaseSeeder.php
+++ laravelapp/database/seeders/DatabaseSeeder.php
@@ -30,6 +30,8 @@ class DatabaseSeeder extends Seeder
             [
                 'name' => 'Red Fox',
                 'email' => 'red_fox@mail.ru',
-                'password' => bcrypt('123abc321')]);
+                'password' => bcrypt('123abc321')
+            ]
+        );
     }
 }
