78d91a1d5da9587832416ce373427c9cd35f087f
Red-Fox-98 <evgeniya.zaitseva.98@gmail.com>
2022-10-26 15:29:00 +0600

    fixed2.0

--- laravelapp/app/Http/Controllers/Api/PlayerController.php
+++ laravelapp/app/Http/Controllers/Api/PlayerController.php
@@ -11,16 +11,6 @@ use App\Http\Requests\Auth\Player\CreateRequest;
 class PlayerController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
      * Show the form for creating a new resource.
      */
     public function create(CreateRequest $request)
@@ -36,61 +26,5 @@ class PlayerController extends Controller
         ]);
 
         return responder()->success(['id' => $player->id])->respond();
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
